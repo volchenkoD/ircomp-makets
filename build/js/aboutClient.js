1fc4e16c05b5aad253b887a973f00f6b73fb9db0
Dmitriy Volchenko <Sverxsro4nik@yandex.ru>
2020-03-24 16:02:16 +0500

    добавлено фотография клиента

--- build/js/aboutClient.js
+++ build/js/aboutClient.js
@@ -3,21 +3,35 @@ let section = document.querySelector('.main__table');
 
 document.querySelector('table').style.display = 'none';
 document.querySelector('.panell__controll').style.display = 'none';
-// добавление кнопок для работы с клиентами
-function buttonBack() {
-    let backButton = document.createElement('button');
-    backButton.innerHTML = 'Назад';
-    backButton.setAttribute('class', 'button__back');
-    return backButton;
+// кнопка вернуться в таблицу
+let buttonBack = createElementClient('button', 'class', 'button__back');
+buttonBack.innerHTML = 'Назад';
+let changeButton = createElementClient('button', 'class', 'button__change');
+changeButton.innerHTML = 'Редактировать профиль';
+
+function createElementClient(name, atrr, value = '') {
+    let elem = document.createElement(`${name}`);
+    elem.setAttribute(`${atrr}`, `${value}`);
+    return elem;
 
 }
 
-function changeUser() {
-    let changeButton = document.createElement('button');
-    changeButton.innerHTML = 'Редактировать профиль';
-    changeButton.setAttribute('class', 'button__change');
-    return changeButton;
+function photoContainer() {
+    let photoContainer = createElementClient('div', 'class', 'user__photo');
+    let img = createElementClient('img', 'src', '/build/img/photo-client.png');
+    let button = createElementClient('button', 'class', 'change__photo');
+    button.innerHTML = 'Сменить фото';
+    photoContainer.append(img);
+    photoContainer.append(button);
+    return photoContainer;
 }
 
-section.append(buttonBack());
-section.append(changeUser());
+function mainContainer() {
+    let mainContainer = createElementClient('div', 'class', 'about__user');
+    let photoPart = photoContainer();
+    mainContainer.append(photoPart);
+    return mainContainer;
+}
+section.append(buttonBack);
+section.append(changeButton);
+section.append(mainContainer());
